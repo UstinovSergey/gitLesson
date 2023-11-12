546b81ebf5245df50503860c3a1673e98dd00851
UstinovSergey <56814109+UstinovSergey@users.noreply.github.com>
2023-11-12 21:53:13 +0800

    Update NewTest.java
    3 to 42

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 public class NewTest {
     void newTest() {
-        int i = 3;
+        int i = 42;
         Assertions.assertTrue(3>2);
         System.out.println("This is new test");
 
