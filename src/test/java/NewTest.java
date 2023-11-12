a5d07408a48bd7573202a485e0bfd76aa139cd35
UstinovSergey <ustinov-sergey@ya.ru>
2023-11-12 21:45:10 +0800

    dima changes

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -3,8 +3,8 @@ import org.junit.jupiter.api.Test;
 
 public class NewTest {
     void newTest() {
-        int i = 3;
-        Assertions.assertTrue(3>2);
+        int i = 10;
+        Assertions.assertTrue(i>2);
         System.out.println("This is new test");
 
     }
